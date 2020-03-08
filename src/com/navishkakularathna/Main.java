0c35fe0e6f38cd7c95718273b63c6db5c138dec0
Navishka Kularathna <navishka.randika@gmail.com>
2020-03-08 13:48:15 +0530

    Method calling

--- src/com/navishkakularathna/Main.java
+++ src/com/navishkakularathna/Main.java
@@ -4,5 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
         boolean gameOver = true;
+        int score = 800;
+        int levelsCompleted = 5;
+        int bonus = 100;
+
+        CalculateScore(gameOver, score, levelsCompleted, bonus);
+    }
+
+    public static void CalculateScore(boolean gameOver, int score, int levelsCompleted, int bonus){
+        if(gameOver){
+            int finalScore = score + (levelsCompleted*bonus);
+            finalScore += 2000;
+            System.out.println("Your final score is: " + finalScore);
+        }
     }
 }
